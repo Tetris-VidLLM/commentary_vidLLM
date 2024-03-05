06d35c645015d803f9f99a6ac259783a59a6df24
ckranig <ckranig@wisc.edu>
2024-03-05 15:58:36 -0600

    finished transcript getter and test

--- get_transcript.py
+++ get_transcript.py
@@ -1,4 +1,14 @@
 from youtube_transcript_api import YouTubeTranscriptApi 
+import re
 
 def get_transcript(youtube_link: str):
-  return None
+  pattern = re.compile(r'\?v=([^&]+)')
+  match = pattern.search(youtube_link)
+  if match:
+    srt = YouTubeTranscriptApi.get_transcript(match.group(1), languages=['en'])
+    return srt
+  return []
+
+if __name__ == "__main__":
+  srt = get_transcript("https://www.youtube.com/watch?v=fYyDQBG_tYc&list=PLA3elidp12Uu_aTr7X0cWnZ7bkB2YK_jQ&index=1")
+  print(srt)
