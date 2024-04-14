7e662d1d745ef78518188fdc3ae093a876baa95a
ckranig <ckranig@wisc.edu>
2024-04-14 14:59:32 -0500

    added function for converting a clip or movie to frames

---
+++ video_to_frames.py
@@ -0,0 +1,19 @@
+from moviepy.editor import VideoFileClip
+import numpy as np
+
+def video_to_frames(video_file: str, interval: int = 1) -> np.ndarray:
+  clip = VideoFileClip(video_file)
+  frames = []
+  duration = clip.duration
+  fps = clip.fps
+  
+  for t in range(0, int(duration), interval):
+    frame = clip.get_frame(t)
+    frames.append(frame)
+    
+  return frames
+
+
+if __name__ == "__main__":
+  video_file = 'testout.mp4'
+  frames = video_to_frames(video_file)
